1430fdd027dd8e646ed1fb6a94e69408d5ba5602
toneloke <guerrerotonyjs@gmail.com>
2017-09-18 15:41:45 -0400

    ajax for all pokemon

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log('app running')
+
+var allPokemon = []
+
+$.ajax({
+  method: 'GET',
+  url: 'http://pokeapi.co/api/v2/pokemon/',
+  success: function (res) {
+    console.log('successfully communicated with api', res)
+    allPokemon = res.results
+    var p = new Pokemon()
+    for (var i = 0; i < allPokemon.length; i++) {
+      p.createPokemon(allPokemon[i])
+    }
+  },
+  error: function (err) {
+    console.log('something went wrong with the api', err)
+  }
+
+})
+
+class Pokemon {
+  constructor () {
+    this.pokeDiv = document.querySelector('.pokemon')
+  }
+  createPokemon (pokemonObj) {
+    this.pokeDiv.innerHTML += `<p><a href="${pokemonObj.url}">${pokemonObj.name}</a></p>`
+  }
+}
+
+console.log('here is an empty array', allPokemon)

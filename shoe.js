36d9e75dff9621257bc6bede4255c7c0ffd34ec1
toneloke <guerrerotonyjs@gmail.com>
2017-09-20 16:51:13 -0400

    post get and delete all done

--- shoe.js
+++ shoe.js
@@ -1,13 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
 function createShoe () {
   event.preventDefault()
   console.log('submiting your shoe data')
 
   let myShoe = {
     shoe: {
-      brand: event.target[0],
-      color: event.target[2],
-      imgURL: event.target[3],
-      size: event.target[1]
+      brand: event.target[0].value,
+      color: event.target[2].value,
+      imgURL: event.target[3].value,
+      size: event.target[1].value
     }
   }
 
@@ -21,5 +32,94 @@ function createShoe () {
 
   })
   .then(res => res.json())
-  .then(shoe => console.log(shoe))
+  .then(shoe => {
+    document.querySelector('.success .success').style.display = 'block'
+  })
+  .catch(err => {
+    console.log('error', err)
+    document.querySelector('.error .error').style.display = 'block'
+    document.getElementById('errmsg').innerHTML = err
+  })
+}
+
+function allShoes () {
+  // clear out the form
+  let container = document.querySelector('.segment')
+  container.innerHTML = ''
+  // retrieve all the shoes from the api
+  fetch('http://localhost:3000/shoes.json')
+    .then(res => res.json())
+    .then(shoes => {
+      let htmlShoes = shoes.map(createShoeItem)
+      container.innerHTML = htmlShoes.join('')
+    })
+    .catch(err => console.log(err))
+  // render them to the screen
+}
+
+function createShoeItem (shoe) {
+  return `
+    <div class="item">
+      <a class="ui tiny image">
+        <img src="${shoe.imgURL}">
+      </a>
+      <div class="content">
+        <a class="header">${shoe.brand}</a>
+         <i id="${shoe.id}" class="trash icon"></i>
+        <div class="description">
+          <p>color:${shoe.color}, size: ${shoe.size}</p>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+document.querySelector('.segment').addEventListener('click', function () {
+  if (event.target.classList.contains('trash')) {
+    let shoeID = event.target.id
+    fetch(`http://localhost:3000/shoes/${shoeID}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }
+    })
+    .then(res => {
+      console.log('worked')
+      allShoes()
+    })
+  }
+})
+
+
+function showForm(){
+  let container = document.querySelector('.segment')
+  container.innerHTML = `  <form class="ui form success error" onsubmit="createShoe()">
+      <div class="field">
+        <label>Shoe Brand</label>
+        <input type="text" name="shoe-brand" placeholder="Shoe Brand">
+      </div>
+      <div class="field">
+        <label>Shoe Size</label>
+        <input type="number" name="shoe-size" placeholder="Shoe Size">
+      </div>
+      <div class="field">
+        <label>Shoe Color</label>
+        <input type="text" name="shoe-color" placeholder="Shoe Color">
+      </div>
+      <div class="field">
+        <label>Shoe Image Link</label>
+        <input type="text" name="shoe-image" placeholder="Shoe Image URL">
+      </div>
+      <div class="ui success message" style="display: none">
+        <div class="header">Form Completed</div>
+        <p>You have succesfully created a show.</p>
+      </div>
+      <div class="ui error message" style="display: none">
+        <div class="header">Action Forbidden</div>
+        <p id="errmsg"></p>
+      </div>
+      <button class="ui button" type="submit">Create Shoe</button>
+    </form>`
+
 }
